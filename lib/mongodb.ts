0baca4a77f62213043eb77d858f23a4d2e72b112
nefesercan-sys <nefesercan@gmail.com>
2026-03-02 11:35:14 +0300

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,20 +3,26 @@ import { MongoClient } from 'mongodb';
 const uri = process.env.MONGODB_URI as string;
 const options = {};
 
-let client;
+let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('Lütfen Vercel paneline MONGODB_URI ekleyin!');
+  throw new Error('Lütfen .env.local dosyasına MONGODB_URI ekleyin');
 }
 
 if (process.env.NODE_ENV === 'development') {
-  if (!(global as any)._mongoClientPromise) {
+  // Geliştirme modunda bağlantının kopmaması için global değişken kullanılır
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClientPromise?: Promise<MongoClient>;
+  };
+
+  if (!globalWithMongo._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    (global as any)._mongoClientPromise = client.connect();
+    globalWithMongo._mongoClientPromise = client.connect();
   }
-  clientPromise = (global as any)._mongoClientPromise;
+  clientPromise = globalWithMongo._mongoClientPromise;
 } else {
+  // Canlı (Production) modunda normal bağlantı kullanılır
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
