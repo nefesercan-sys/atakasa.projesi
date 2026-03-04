f1d36ff853045c410da94ea9219c9d1b4e21f5da
nefesercan-sys <nefesercan@gmail.com>
2026-03-04 13:57:40 +0300

    Update Product.ts

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,10 +1,12 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const ProductSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  status: { type: String, default: "available" }
-}, { timestamps: true })
+const productSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  ownerEmail: { type: String, required: true },
+  image: { type: String, default: "https://placehold.co/600x400/030712/00f260?text=ATAKASA" },
+  status: { type: String, default: "aktif" }
+}, { timestamps: true });
 
-export default mongoose.models.Product || mongoose.model("Product", ProductSchema)
+export default mongoose.models.Product || mongoose.model("Product", productSchema);
