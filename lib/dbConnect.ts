8f9432f57c7030ced8bd4c426cdd1cb37a0a733f
nefesercan-sys <nefesercan@gmail.com>
2026-03-04 12:11:17 +0300

    Update dbConnect.ts

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,26 +1,25 @@
-// lib/dbConnect.ts
-
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
-  throw new Error("Please define the MONGODB_URI environment variable");
+  throw new Error("MONGODB_URI tanımlı değil");
 }
 
 let cached = (global as any).mongoose;
 
 if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
 
 async function dbConnect() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
-      return mongoose;
-    });
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
