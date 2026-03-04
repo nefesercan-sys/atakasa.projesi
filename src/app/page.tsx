974289125c0353640e6299037312fc788cdd3882
nefesercan-sys <nefesercan@gmail.com>
2026-03-04 12:25:49 +0300

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{
+      display: "flex",
+      height: "100vh",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+      fontFamily: "sans-serif"
+    }}>
+      <h1 style={{ fontSize: 40 }}>Atakasa Yayında 🚀</h1>
+      <p>Dijital Takas & Hizmet Ekosistemi</p>
+    </main>
+  )
+}
