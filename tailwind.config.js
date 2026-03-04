56bbb74913d776272904a1f0e0894fd7c06517b4
nefesercan-sys <nefesercan@gmail.com>
2026-03-04 22:33:17 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,21 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // 🚀 src içindeki HER ŞEYİ tara
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // 🚀 (Önlem amaçlı) ana dizindeki app klasörünü tara
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
       animation: {
         'marquee': 'marquee 30s linear infinite',
-        'marquee-fast': 'marquee 15s linear infinite',
       },
       keyframes: {
         marquee: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(-50%)' },
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
         }
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
