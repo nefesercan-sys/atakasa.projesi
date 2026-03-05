82da279c5850857c4b2e57b17f6697eb3418d481
nefesercan-sys <nefesercan@gmail.com>
2026-03-05 15:52:36 +0300

    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,8 +1,17 @@
-import mongoose from "mongoose"
+import mongoose, { Schema, models } from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-}, { timestamps: true })
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    role: { type: String, default: "Pro Broker" },
+    image: { type: String },
+    toplamHacim: { type: Number, default: 0 },
+    basariliIslem: { type: Number, default: 0 }
+  },
+  { timestamps: true } // Ne zaman kayıt olduğunu otomatik tutar
+);
 
-export default mongoose.models.User || mongoose.model("User", UserSchema)
+const User = models.User || mongoose.model("User", userSchema);
+export default User;
