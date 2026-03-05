78bda3c66c280a18e9a75652440dd215cb6d03d2
nefesercan-sys <nefesercan@gmail.com>
2026-03-05 04:25:48 +0300

    Update route.ts

--- src/app/api/trades/route.ts
+++ src/app/api/trades/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { connectDB } from "../../../lib/mongodb";
+import  connectDB  from "../../../lib/mongodb";
 import Trade from "../../../models/Trade";
 
 // 📥 GET: Kullanıcıya Gelen ve Giden Takas Tekliflerini Çek
