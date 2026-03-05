a68b5387c25248cac0d585e382dc2a7511a18f89
nefesercan-sys <nefesercan@gmail.com>
2026-03-05 16:09:43 +0300

    Update route.ts

--- src/app/api/kayit/route.ts
+++ src/app/api/kayit/route.ts
@@ -1,25 +1,22 @@
 import { NextResponse } from "next/server";
-import { connectMongoDB } from "../../../../lib/mongodb";
-import User from "../../../../models/User";
+// DÜZELTİLDİ: 3 tane ../ olacak
+import { connectMongoDB } from "../../../lib/mongodb";
+import User from "../../../models/User";
 import bcrypt from "bcryptjs";
 
 export async function POST(req: Request) {
   try {
     const { name, email, password } = await req.json();
     
-    // Veritabanına bağlan
     await connectMongoDB();
     
-    // Bu mail daha önce alınmış mı kontrol et
     const userExists = await User.findOne({ email });
     if (userExists) {
       return NextResponse.json({ message: "SİBER HATA: Bu e-posta ağda zaten mevcut." }, { status: 400 });
     }
     
-    // Şifreyi siber kriptolama ile şifrele (Hacklenemez yap)
     const hashedPassword = await bcrypt.hash(password, 10);
     
-    // Kullanıcıyı yarat ve veritabanına mühürle
     await User.create({ name, email, password: hashedPassword });
     
     return NextResponse.json({ message: "Siber kimlik başarıyla oluşturuldu." }, { status: 201 });
