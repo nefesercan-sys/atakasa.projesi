a2eb1db980b26cd82b0018d8166d932c249572a0
nefesercan-sys <nefesercan@gmail.com>
2026-03-05 04:25:17 +0300

    Update route.ts

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { connectDB } from "../../../lib/mongodb";
+import  connectDB  from "../../../lib/mongodb";
 import Product from "../../../models/Product";
 // 🚨 VERCEL ÖNBELLEK KIRICI: Yeni ürün eklendiğinde anında görünmesini sağlar
 export const dynamic = "force-dynamic";
