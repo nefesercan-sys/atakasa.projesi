91c8f8b68235c7c128d17c9deed52fb20cda15dd
nefesercan-sys <nefesercan@gmail.com>
2026-03-07 18:43:53 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ['res.cloudinary.com'], // Siber resim bulutu izni
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN' // Sitenin başka sitelerde iframe olarak açılmasını engeller (Clickjacking koruması)
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          }
+        ],
+      },
+    ]
   },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '50mb', // 50MB'a kadar veri enjeksiyonu izni
-    },
-  },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
