9bca1bdcccb1ba26daf00879e61c4f465b2f595e
nefesercan-sys <nefesercan@gmail.com>
2026-03-11 00:18:59 +0300

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,6 @@
+import Link from 'next/link';
+
+// Footer menüsünün içine bir yere şu satırı ekle:
+<Link href="/sozlesme" className="text-slate-400 hover:text-[#00f260] transition-colors text-xs font-bold uppercase tracking-widest">
+   Kullanıcı Sözleşmesi ve KVKK
+</Link>
