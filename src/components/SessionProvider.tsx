f3322a953e71a3e781883c64f1f49511db290446
nefesercan-sys <nefesercan@gmail.com>
2026-03-17 13:07:04 +0300

    Create SessionProvider.tsx

---
+++ src/components/SessionProvider.tsx
@@ -0,0 +1,18 @@
+// src/components/SessionProvider.tsx
+"use client";
+
+import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
+import type { Session } from "next-auth";
+
+interface Props {
+  children: React.ReactNode;
+  session?: Session | null;
+}
+
+export default function SessionProvider({ children, session }: Props) {
+  return (
+    <NextAuthSessionProvider session={session}>
+      {children}
+    </NextAuthSessionProvider>
+  );
+}
