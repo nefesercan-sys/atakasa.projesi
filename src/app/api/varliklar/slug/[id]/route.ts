f89a7b18127ac192a6ca9269b5aeca439eb04b4a
nefesercan-sys <nefesercan@gmail.com>
2026-03-21 20:07:41 +0300

    Update route.ts

--- src/app/api/varliklar/slug/[id]/route.ts
+++ src/app/api/varliklar/slug/[id]/route.ts
@@ -8,7 +8,7 @@ export async function GET(
   { params }: { params: { id: string } }
 ) {
   try {
-    await connectToDatabase();
+    await connectMongoDB(); //
     const varlik = await Varlik
       .findById(params.id)
       .select("slug")
